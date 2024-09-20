9e43345fff6aa0ba2d7a97a0e641f7c94e5e749a
fedkin@bramblingtech.com <fedkin@bramblingtech.com>
2024-09-20 09:19:00 +0300

    elasticsearch - not working :) need to setting up

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
@@ -24,11 +26,16 @@ import { AuthenticationModule } from './authentication/authentication.module';
         AWS_SECRET_ACCESS_KEY: Joi.string().required(),
         AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
         AWS_PRIVATE_BUCKET_NAME: Joi.string().required(),
+        ELASTICSEARCH_NODE: Joi.string(),
+        ELASTICSEARCH_USERNAME: Joi.string(),
+        ELASTICSEARCH_PASSWORD: Joi.string(),
       }),
     }),
     DatabaseModule,
     AuthenticationModule,
     UsersModule,
+    CategoriesModule,
+    SearchModule,
   ],
   controllers: [],
   providers: [],
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,15 +3,17 @@ import CreatePostDto from './dto/createPost.dto';
 import Post from './post.entity';
 import UpdatePostDto from './dto/updatePost.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import PostNotFoundException from './exceptions/postNotFound.exception';
 import User from '../users/user.entity';
+import PostsSearchService from './postsSearch.service';
 
 @Injectable()
 export default class PostsService {
   constructor(
     @InjectRepository(Post)
     private postsRepository: Repository<Post>,
+    private postsSearchService: PostsSearchService,
   ) {}
 
   getAllPosts() {
@@ -30,12 +32,15 @@ export default class PostsService {
   }
 
   async createPost(post: CreatePostDto, user: User) {
-    const newPost = await this.postsRepository.create({
+    const newPost = this.postsRepository.create({
       ...post,
       author: user,
     });
-    await this.postsRepository.save(newPost);
-    return newPost;
+    console.log(newPost);
+    const savedPost = await this.postsRepository.save(newPost);
+    console.log(savedPost);
+    await this.postsSearchService.indexPost(savedPost);
+    return savedPost;
   }
 
   async updatePost(id: number, post: UpdatePostDto) {
@@ -45,6 +50,7 @@ export default class PostsService {
       relations: ['author'],
     });
     if (updatedPost) {
+      await this.postsSearchService.update(updatedPost);
       return updatedPost;
     }
     throw new PostNotFoundException(id);
@@ -55,5 +61,17 @@ export default class PostsService {
     if (!deleteResponse.affected) {
       throw new PostNotFoundException(id);
     }
+    await this.postsSearchService.remove(id);
+  }
+
+  async searchForPosts(text: string) {
+    const results = await this.postsSearchService.search(text);
+    const ids = results.map((result) => result.id);
+    if (!ids.length) {
+      return [];
+    }
+    return this.postsRepository.find({
+      where: { id: In(ids) },
+    });
   }
 }
